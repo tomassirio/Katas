0ebdf90d231a3d242e37d3dc2013c6f9da1548dc
tomassirio <tomassirio@gmail.com>
2025-08-04 11:17:51 +0200

    Implement turnOff

--- Christmas-Lights/src/main/java/com/tomassirio/practice/service/impl/LightServiceImpl.java
+++ Christmas-Lights/src/main/java/com/tomassirio/practice/service/impl/LightServiceImpl.java
@@ -25,7 +25,15 @@ public class LightServiceImpl implements LightService {
 
     @Override
     public void turnOff(Coordinate from, Coordinate to, LightMatrix lightMatrix) {
+        // Ensure coordinates are within bounds
+        Coordinate boundedFrom = boundFromCoordinate(from);
+        Coordinate boundedTo = boundToCoordinate(to, lightMatrix.getMatrix().length);
 
+        for (int i = boundedFrom.x(); i <= boundedTo.x(); i++) {
+            for (int j = boundedFrom.y(); j <= boundedTo.y(); j++) {
+                lightMatrix.getMatrix()[i][j] = Boolean.FALSE;
+            }
+        }
     }
 
     @Override
--- Christmas-Lights/src/test/java/com/tomassirio/practice/service/LightServiceImplTest.java
+++ Christmas-Lights/src/test/java/com/tomassirio/practice/service/LightServiceImplTest.java
@@ -49,4 +49,21 @@ public class LightServiceImplTest {
             row -> Arrays.stream(row).forEach(light -> assertThat(light).isTrue())
         );
     }
+
+    @Test
+    public void test_turnOff() {
+        // Given
+        LightMatrix lightMatrix = new LightMatrix(5, 5);
+        Coordinate from = new Coordinate(0, 0);
+        Coordinate to = new Coordinate(4, 4);
+        lightService.turnOn(from, to, lightMatrix);
+
+        // When
+        lightService.turnOff(from, to, lightMatrix);
+
+        // Then
+        Arrays.stream(lightMatrix.getMatrix()).forEach(
+            row -> Arrays.stream(row).forEach(light -> assertThat(light).isFalse())
+        );
+    }
 }
