db372195d46dcac4566ff1d112e0362145195b50
tomassirio <tomassirio@gmail.com>
2025-08-04 14:51:30 +0200

    Add Instruction set for exercise 1

--- Christmas-Lights/src/main/java/com/tomassirio/practice/App.java
+++ Christmas-Lights/src/main/java/com/tomassirio/practice/App.java
@@ -1,13 +1,31 @@
 package com.tomassirio.practice;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.tomassirio.practice.model.Coordinate;
+import com.tomassirio.practice.model.LightMatrix;
+import com.tomassirio.practice.service.LightService;
+import com.tomassirio.practice.service.MatrixOperator;
+import com.tomassirio.practice.service.impl.LightServiceImpl;
+import com.tomassirio.practice.service.impl.MatrixOperatorImpl;
+
+public class App {
+    public static void main( String[] args ) {
+        LightService lightService = new LightServiceImpl();
+        MatrixOperator matrixOperator = new MatrixOperatorImpl();
+        final Integer MATRIX_ROW_SIZE = 1_000;
+
+        LightMatrix lightMatrix = new LightMatrix(MATRIX_ROW_SIZE, MATRIX_ROW_SIZE);
+
+        lightService.turnOn(new Coordinate(887, 9), new Coordinate(959, 629), lightMatrix);
+        lightService.turnOn(new Coordinate(454, 398), new Coordinate(844, 448), lightMatrix);
+        lightService.turnOff(new Coordinate(539, 243), new Coordinate(559, 965), lightMatrix);
+        lightService.turnOff(new Coordinate(370, 819), new Coordinate(676, 868), lightMatrix);
+        lightService.turnOff(new Coordinate(145, 40), new Coordinate(370, 997), lightMatrix);
+        lightService.turnOff(new Coordinate(301, 3), new Coordinate(808, 453), lightMatrix);
+        lightService.turnOn(new Coordinate(351, 678), new Coordinate(951, 908), lightMatrix);
+        lightService.toggle(new Coordinate(720, 196), new Coordinate(897, 994), lightMatrix);
+        lightService.toggle(new Coordinate(831, 394), new Coordinate(904, 860), lightMatrix);
+
+        Integer turnedOnLights = matrixOperator.countLights(lightMatrix, Boolean.TRUE);
+        System.out.println("Number of lights turned on: " + turnedOnLights);
     }
 }
--- Christmas-Lights/src/test/java/com/tomassirio/practice/service/IntegrationTest.java
+++ Christmas-Lights/src/test/java/com/tomassirio/practice/service/IntegrationTest.java
@@ -4,6 +4,7 @@ import com.tomassirio.practice.model.Coordinate;
 import com.tomassirio.practice.model.LightMatrix;
 import com.tomassirio.practice.service.impl.LightServiceImpl;
 import com.tomassirio.practice.service.impl.MatrixOperatorImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -58,6 +59,26 @@ public class IntegrationTest {
 
         // Then
         assertThat(turnedOn).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void testAllInstructionsInOrderResultsIn230022TurnedOnLights() {
+        // Given
+        LightMatrix lightMatrix = new LightMatrix(1_000, 1_000);
+
+        // When - Execute all instructions in the specified order
+        lightService.turnOn(new Coordinate(887, 9), new Coordinate(959, 629), lightMatrix);
+        lightService.turnOn(new Coordinate(454, 398), new Coordinate(844, 448), lightMatrix);
+        lightService.turnOff(new Coordinate(539, 243), new Coordinate(559, 965), lightMatrix);
+        lightService.turnOff(new Coordinate(370, 819), new Coordinate(676, 868), lightMatrix);
+        lightService.turnOff(new Coordinate(145, 40), new Coordinate(370, 997), lightMatrix);
+        lightService.turnOff(new Coordinate(301, 3), new Coordinate(808, 453), lightMatrix);
+        lightService.turnOn(new Coordinate(351, 678), new Coordinate(951, 908), lightMatrix);
+        lightService.toggle(new Coordinate(720, 196), new Coordinate(897, 994), lightMatrix);
+        lightService.toggle(new Coordinate(831, 394), new Coordinate(904, 860), lightMatrix);
+
+        Integer turnedOn = matrixOperator.countLights(lightMatrix, Boolean.TRUE);
+        assertThat(turnedOn).isEqualTo(230022);
     }
 
     public static Stream<Arguments> turnOnInstructions() {
